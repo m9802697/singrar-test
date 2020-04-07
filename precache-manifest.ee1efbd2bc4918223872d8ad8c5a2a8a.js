3f21766c3186f723356c2e2dc5b7a67790f772b1
m9802697 <pelixo.lamy@gmail.com>
2020-04-07 14:39:14 +0100

    Updates

---
+++ precache-manifest.ee1efbd2bc4918223872d8ad8c5a2a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3184c1cd18e64599665363d41eb9e0",
+    "url": "/singrar-test/index.html"
+  },
+  {
+    "revision": "066f2cf9284fe8fcab08",
+    "url": "/singrar-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7f77e054f652a145a22",
+    "url": "/singrar-test/static/js/2.aba2e33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/singrar-test/static/js/2.aba2e33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066f2cf9284fe8fcab08",
+    "url": "/singrar-test/static/js/main.c2a745e5.chunk.js"
+  },
+  {
+    "revision": "d07fd7dd35d335717711",
+    "url": "/singrar-test/static/js/runtime-main.902fcd94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/singrar-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
